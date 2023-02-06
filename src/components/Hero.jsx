f8ec43a0ae7e3dfbb760bd57aeb1c20b11dae1a3
Syed Wajahat Ali <wajahat.klk25.com>
2023-02-06 23:39:10 +0100

    creates brach

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,6 +12,7 @@ import airbnbLogo from "../images/airbnb-logo.png";
 export default function Hero() {
   return (
     <section className="hero-container">
+      <img className="hero-logo" src={airbnbLogo} alt="airbnb-logo" />
       <div className="hero">
         <img src={img1} alt="hero-img" />
         <img src={img2} alt="hero-img" />
@@ -23,7 +24,6 @@ export default function Hero() {
         <img src={img8} alt="hero-img" />
       </div>
       <div className="hero-text">
-        <img className="hero-logo" src={airbnbLogo} alt="airbnb-logo" />
         <span className="text-line"></span>
         <h1 className="hero-title">Nº1 online marketplace</h1>
         <h3>Short-term homestays and experiences all around the world.</h3>
