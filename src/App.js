a77da3fd29a007f7f70166a0302bb4b6bb34f7fb
Syed Wajahat Ali <wajahat.klk25.com>
2023-02-20 00:07:37 +0100

    adds styles

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import "./App.css";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
+import data from "./data";
 
 function App() {
+  const cards = data.map((data) => {
+    return <Card key={data.id} data={data} />;
+  });
+
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <section className="cards-list">{cards}</section>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,20 @@
-import React from "react";
-import tenent from "../images/andrea-davis-IWfe63thJxk-unsplash.jpg";
-import tenent2 from "../images/meritt-thomas-_YxDGcDm4Hs-unsplash.jpg";
-import tenent3 from "../images/benjamin-suter-S-x1RbdoCtU-unsplash.jpg";
-import tenent4 from "../images/karsten-winegeart-sStahKEhT9w-unsplash.jpg";
-import tenent5 from "../images/andrea-davis-IWfe63thJxk-unsplash.jpg";
 import star from "../images/star.png";
-export default function Card() {
+export default function Card({ data }) {
   return (
-    <>
+    <div className="card--container">
       <div className="card">
-        <img src={tenent} alt="card-img" className="card--img" />
+        <img src={data.img} alt="card-img" className="card--img" />
         <div className="card--stats">
           <img src={star} alt="star" />
-          <span>5.0</span>
-          <span className="grey">(6) ·</span>
-          <span className="grey">SPAIN</span>
+          <span>{data.rating}</span>
+          <span className="grey">({data.reviewCount}) ·</span>
+          <span className="grey">{data.country}</span>
         </div>
-        <p>Barcelona center</p>
+        <p>{data.title}</p>
         <p>
-          <span className="bold">FROM 150€</span> / night
+          <span className="bold">FROM {data.price}€</span> / night
         </p>
       </div>
-    </>
+    </div>
   );
 }
